d222cc7125912819a678aec7e2b729ed32041357
Khem Myrick <khem.myrick@gmail.com>
2018-10-30 12:44:01 -0700

    Added test of functionality behind search view function.

--- minerals/tests.py
+++ minerals/tests.py
@@ -2,6 +2,7 @@ import random
 import unittest
 
 from django.urls import reverse
+from django.db.models import Q
 from django.test import TestCase
 from django.template import Context, Template
 
@@ -352,9 +353,28 @@ class ViewsTests(TestCase):
                          resp.context['minerals'])
 
     def test_search_view(self):
-        resp = self.client.get(reverse('minerals:search')
         term = 'Rose Quartz faces Pink Diamond through a pane of glass'
-        self.assertEqual(resp.status_code, 200)  # FAILS. How do I test this?
+        minerals = Mineral.objects.filter(
+            Q(name__icontains=term)|
+            Q(group__name__icontains=term)|
+            Q(imgcap__icontains=term)|
+            Q(formula__icontains=term)|
+            Q(category__icontains=term)|
+            Q(strunz_classification__icontains=term)|
+            Q(color__icontains=term)|
+            Q(crystal_system__icontains=term)|
+            Q(unit_cell__icontains=term)|
+            Q(crystal_symmetry__icontains=term)|
+            Q(cleavage__icontains=term)|
+            Q(luster__icontains=term)|
+            Q(mohs_scale_hardness__icontains=term)|
+            Q(streak__icontains=term)|
+            Q(diaphaneity__icontains=term)|
+            Q(optical_properties__icontains=term)|
+            Q(refractive_index__icontains=term)|
+            Q(crystal_habit__icontains=term)|
+            Q(specific_gravity__icontains=term)
+        ).values('name', 'group', 'id')
         mineral1 = {'name': self.rosequartz.name,
                     'group': self.rosequartz.group.id,
                     'id': self.rosequartz.id}
@@ -362,9 +382,9 @@ class ViewsTests(TestCase):
                     'group': self.garnet.group.id,
                     'id': self.garnet.id}
         self.assertIn(mineral1,
-                      resp.context['minerals'])
+                      minerals)
         self.assertNotIn(mineral2,
-                         resp.context['minerals'])
+                         minerals)
 
 class MineralExtrasTests(TestCase):
     def setUp(self):
