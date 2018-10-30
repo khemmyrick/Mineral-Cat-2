ae9b22af7cd63a82093e969895f597d80262f8f8
Khem Myrick <khem.myrick@gmail.com>
2018-10-30 12:45:22 -0700

    Edited mineralcat views.

--- mineralcat/views.py
+++ mineralcat/views.py
@@ -6,9 +6,3 @@ from minerals import views
 
 def index(request):
     return views.by_alpha(request, 'A')
-#    groups = Group.objects.all()
-#    group = ''
-#    return render(request,
-#                  'minerals/index.html',
-#                  {'groups': groups,
-#                   'group': group})
